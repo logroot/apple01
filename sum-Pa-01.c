ed75c992349f662452e246e8a01779d9a5b2df7b
pablito234 <grossjessy680@gmail.com>
2022-04-03 11:57:05 +0200

    Summe Math

---
+++ sum-Pa-01.c
@@ -0,0 +1,22 @@
+
+// Aufgabe : Summe von zwei Ganzzahlen
+// Autor : jutsuman (Github name)
+// Datum : 03.04.20022
+
+#include <stdio.h>
+
+int main() {
+        int a, b, summe;
+
+        printf("Geben Sie bitte eine Zahl ein : ");
+        scanf("%i", &a);
+        printf("Geben Sie bitte eine andere Zahl ein : ");
+        scanf("%i", &b);
+
+        // Wir berechnen die Summe von zwei Ganzzahlen
+        summe = sum(a, b);
+
+        printf("Die Summe von %i und %i ist : %i\n", a, b, summe);      
+
+        return 0;
+}
